201f56005123a6a31c3ef99e29fc0ace2479914c
Mia Frank <31712634+miafrank@users.noreply.github.com>
2020-07-21 13:36:21 -0500

    added starting expo file

--- App.js
+++ App.js
@@ -1,33 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.screen}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <TextInput placeholder="Course Goal" style={styles.inputContainer} />
-        <Button title="Add" />
-      </View>
-      <View></View>
-      <StatusBar style="auto" />
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    padding: 50,
-  },
-  inputContainer: {
-    width: "80%",
-    borderBottomColor: "black",
-    borderBottomWidth: 1,
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
