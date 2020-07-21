4b3e343d5eded726a9d2d8419ab612011fa6d5b2
Mia Frank <31712634+miafrank@users.noreply.github.com>
2020-07-21 15:28:25 -0500

    added card component, screen component, and color theme

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,7 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import Colors from '../constants/colors';
 
 const Header = (props) => {
   return (
@@ -11,16 +13,16 @@ const Header = (props) => {
 
 const styles = StyleSheet.create({
   header: {
-    width: "100%",
+    width: '100%',
     height: 90,
     paddingTop: 36,
-    backgroundColor: "#f7287b",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: Colors.primary,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
   headerTitle: {
-    color: "black",
+    color: 'black',
     fontSize: 18,
   },
 });
